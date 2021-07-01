d2d4ada834aa1e1f8a02780d18e2e7acd6a4565e
Parvin <parvin.altae@gmail.com>
2021-06-30 21:17:16 -0400

    ready to push

--- src/test/java/userstory102.java
+++ src/test/java/userstory102.java
@@ -1,5 +1,6 @@
 public class userstory102 {
     public static void main(String[] args) {
         System.out.println("new story");
+        //ready to push this branch to remote and open pull request
     }
 }
